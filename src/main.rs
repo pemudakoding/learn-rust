fac018b93c32bc14b233786cdb67acb96e4b2e1a
pemudakoding <pemudakoding@gmail.com>
2024-11-14 00:29:38 +0800

    learn variable

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,37 @@ fn main() {
 fn hello_test() {
     println!("Hello Test")
 }
+
+#[test]
+fn test_variable() {
+    let name: &str = "Stiven Stefanus Triski Katuuk";
+
+    println!("Hello {}", name);
+}
+
+#[test]
+fn test_variable_mut() {
+    let mut name = "Stiven Stefanus Triski Katuuk";
+    println!("Hello {}", name);
+
+    name = "Risna Sandi Aulia";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn static_typing() {
+    let name = "Stiven Stefanus Triski Katuuk";
+    println!("Hello {}", name);
+
+    // name = "10";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn shadowing() {
+    let name = "Stiven Stefanus Triski Katuuk";
+    println!("Hello {}", name);
+
+    let name = 10;
+    println!("Hello {}", name);
+}
