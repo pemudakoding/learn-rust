4ec23f1f149371cbddca3142c03b211b60dbb432
pemudakoding <pemudakoding@gmail.com>
2024-11-14 22:52:46 +0800

    learn number type data

--- src/main.rs
+++ src/main.rs
@@ -40,3 +40,37 @@ fn shadowing() {
     let name = 10;
     println!("Hello {}", name);
 }
+
+#[test]
+fn explicit() {
+    let age: i32 = 20;
+
+    println!("{}", age);
+}
+
+#[test]
+fn number() {
+    let a: i8 = 10;
+    println!("{}", a);
+
+    let b: f32 = 10.5;
+    println!("{}", b);
+}
+
+#[test]
+fn number_conversion() {
+    let a: i8 = 10;
+    println!("{}", a);
+
+    let b: i16 = a as i16;
+    println!("{}", b);
+
+    let c: i32 = b as i32;
+    println!("{}", c);
+
+    let d: i64 = 10000000000000;
+    println!("{}", d);
+
+    let e: i8 = d as i8;
+    println!("{}", e);
+}
