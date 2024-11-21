ede5883c1a3b8e4bbf143f23bf1794dd39e4a211
pemudakoding <pemudakoding@gmail.com>
2024-11-22 00:38:04 +0800

    slice

--- src/main.rs
+++ src/main.rs
@@ -490,3 +490,18 @@ fn test_change_value() {
 
     println!("{}", value);
 }
+
+
+#[test]
+fn slice_reference() {
+    let array: [i32; 10] = [1,2,3,4,5,6,7,8,9,10];
+
+    let slice1: &[i32] = &array[..];
+    println!("{:?}", slice1);
+
+    let slice2: &[i32] = &array[0..5];
+    println!("{:?}", slice2);
+
+    let slice3: &[i32] = &array[5..];
+    println!("{:?}", slice3);
+}
