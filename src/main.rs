af49fd34a528174ab99c5656043b428d08d3cd68
pemudakoding <pemudakoding@gmail.com>
2024-12-04 22:50:12 +0800

    learn comparison

--- src/main.rs
+++ src/main.rs
@@ -1012,3 +1012,25 @@ fn test_option() {
     let result: Option<i32> = double(None);
     println!("{:?}", result);
 }
+
+impl PartialEq for Apple {
+    fn eq(&self, other: &Self) -> bool {
+        self.quantity == other.quantity
+    }
+}
+
+impl PartialOrd for Apple {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.quantity.partial_cmp(&other.quantity)
+    }
+}
+
+#[test]
+fn test_compare() {
+    let apple1 = Apple{quantity: 10};
+    let apple2 = Apple{quantity: 20};
+
+    println!("Apple1 == Apple2 : {}", apple1 == apple2);
+    println!("Apple1 < Apple2 : {}", apple1 < apple2);
+    println!("Apple1 > Apple2 : {}", apple1 > apple2);
+}
