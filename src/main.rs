01981c402937b43e5eba91c614d1e50e323af022
pemudakoding <pemudakoding@gmail.com>
2024-12-08 00:45:41 +0800

    learn lifetime annotation

--- src/main.rs
+++ src/main.rs
@@ -1308,3 +1308,89 @@ fn test_recoverable_error() {
         }
     }
 }
+
+#[test]
+fn test_dangling_reference() {
+    let r: &i32;
+    {
+        let x = 5;
+        // r = &x;
+    }
+
+    r = &20;
+    println!("{}", r);
+}
+
+fn longest<'a>(value1: &'a str, value2: &'a str) -> &'a str {
+    if value1.len() > value2.len() {
+        value1
+    } else {
+        value2
+    }
+}
+
+#[test]
+fn test_lifetime_annotation() {
+    let value1 = "Eko";
+    let value2 = "Kurniawan";
+    let result = longest(value1, value2);
+
+    println!("{}", result);
+}
+
+#[test]
+fn test_lifetime_annotation_dangling_reference() {
+    let string1 = String::from("Eko");
+    let string2 = String::from("Kurniawan");
+    let result: &str;
+
+    {
+        result = longest(string1.as_str(), string2.as_str());
+    }
+
+    println!("{}", result);
+}
+
+struct Student<'a, 'b> {
+    name: &'a str,
+    last_name: &'b str,
+}
+
+impl<'a, 'b> Student<'a, 'b> {
+    fn longest_name(&self, student: &Student<'a, '_>) -> &'a str {
+        if self.name.len() > student.name.len() {
+            self.name
+        } else {
+            student.name
+        }
+    }
+}
+
+fn longest_student_name<'a, 'b>(student1: &Student<'a, '_>, student2: &Student<'a, '_>) -> &'a str {
+    if student1.name.len() > student2.name.len() {
+        student1.name
+    } else {
+        student2.name
+    }
+}
+
+#[test]
+fn test_student() {
+    let student = Student {
+        name: "Eko",
+        last_name: "Kurniawan",
+    };
+
+    let student2 = Student {
+        name: "Budi",
+        last_name: "Nugraha",
+    };
+    
+    let result = longest_student_name(&student, &student2);
+    
+    println!("{}", result);
+    
+    let result = student.longest_name(&student2);
+    
+    println!("{}", result);
+}
